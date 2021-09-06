d5ecdf402fe17c8653c08cc025e1144f418101fe
Stephen <sdarcy29@hotmail.com>
2021-09-06 18:45:30 +0000

    Add code for increment and decrement scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,8 +43,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
             if (isCorrect) {
                 alert('Hey! You got it right:)')
+                incrementScore();
             } else {
                 alert(`Awwww..... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+                incrementWrongAnswer();
             }
 
             runGame(calculatedAnswer[1]);
@@ -68,8 +70,20 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-
+/**
+ * Gets the current score and increments it by 1
+ */
         function incrementScore() {
+
+            let oldScore = parseInt(document.getElementById('score').innerText);
+            document.getElementById('score').innerText = ++oldScore;
+
+        }
+
+        function incrementWrongAnswer() {
+
+            let oldScore = parseInt(document.getElementById('incorrect').innerText);
+            document.getElementById('incorrect').innerText = ++oldScore;
 
         }
 
